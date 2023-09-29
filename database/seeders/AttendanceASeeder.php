eca7ea609e70f836d80e444366b04231fdb7a60a
Koykoy200078 <franc200078@gmail.com>
2023-09-29 13:20:06 +0800

    update

--- database/seeders/AttendanceASeeder.php
+++ database/seeders/AttendanceASeeder.php
@@ -12,79 +12,43 @@ class AttendanceASeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        $names = [
-            'Cañete, John Adnhel E.',
-            'Galleon Oliver P.',
-            'Jumawan, Lysa Mae A. ',
-            'Ceriales, Aiko',
-            'Almirante, Jann Louie',
-            'Arceño, Jayson',
-            'Cafino, Randy',
-            'Jonathan Cantal Jr.',
-            'Epan Christian Ray',
-            'Austero Crank Neil R.',
-            'Radoc, Cyrell Jane E.',
-            'Francis Clinton Dorio',
-            'John Howard Elmaco',
-            'Lee Cliford Eluna',
-            'Wingel Antonnette Ention',
-            'ESTRELLADO, JAY, Z.',
-            'Paster, Jerico P.',
-            'Deatras, Keizharra',
-            'Delara, Kriste Jo',
-            'Calpis, Lorenzo R. Jr.',
-            'Santillan Lorie Ann Mae',
-            'Pasague Ma. Cristina M.',
-            'Ronin Allyson Manso',
-            'Des Jozef Mariano',
-            'Magsayo, Mary Paola',
-            'Sabejon Matt Ian',
-            'Bantillo Phoebe Mariz',
-            'Tubongbanua Reden D.',
-            'RUSIANA Shane Mark',
+        $data = [
+            ['school_id' => '202101696', 'name' => 'Almirante, Jann Louie Q.', 'date' => null],
+            ['school_id' => '202203013', 'name' => 'Arceño, Jayson E.', 'date' => null],
+            ['school_id' => '202102022', 'name' => 'Austero, Crank Neil R.', 'date' => null],
+            ['school_id' => '202202286', 'name' => 'Bantillo, Phoebe Mariz Q.', 'date' => null],
+            ['school_id' => '202120854', 'name' => 'Bondad, Don Marc P.', 'date' => null], // absent
+            ['school_id' => '201905138', 'name' => 'Cafino, Randy S.', 'date' => null], // absent
+            ['school_id' => '202202291', 'name' => 'Calpis, Lorenzo Jr. R.', 'date' => null],
+            ['school_id' => '202102046', 'name' => 'Cantal, Jonathan Jr. U.', 'date' => null],
+            ['school_id' => '202202292', 'name' => 'Cañete, John Adnhel E.', 'date' => null],
+            ['school_id' => '202200368', 'name' => 'Ceriales, Aiko V.', 'date' => null],
+            ['school_id' => '202202298', 'name' => 'Deatras, Keizharra B.', 'date' => null],
+            ['school_id' => '202202299', 'name' => 'Delara, Kriste Jo P.', 'date' => null],
+            ['school_id' => '202202300', 'name' => 'Dorio, Francis Clinton G.', 'date' => null],
+            ['school_id' => '202202301', 'name' => 'Elmaco, John Howard B.', 'date' => null],
+            ['school_id' => '202000117', 'name' => 'Eluna, Lee Cliford P.', 'date' => null],
+            ['school_id' => '202001497', 'name' => 'Ention, Wingel Antonnette T.', 'date' => null], // absent
+            ['school_id' => '202120872', 'name' => 'Epan, Christian Ray B.', 'date' => null], // absent
+            ['school_id' => '202120874', 'name' => 'Estrellado, Jay Z.', 'date' => null], // absent
+            ['school_id' => '2021214678', 'name' => 'Galleon, Oliver P.', 'date' => null],
+            ['school_id' => '202120882', 'name' => 'Jumawan, Lysa Mae A.', 'date' => null],
+            ['school_id' => '202120887', 'name' => 'Magsayo, Mary Paola', 'date' => null],
+            ['school_id' => '202121001', 'name' => 'Manso, Ronin Allyson Y.', 'date' => null],
+            ['school_id' => '202202317', 'name' => 'Mariano, Des Jozef M.', 'date' => null],
+            ['school_id' => '202202326', 'name' => 'Pasague, Ma. Cristina M.', 'date' => null],
+            ['school_id' => '202101655', 'name' => 'Paster, Jerico P.', 'date' => null],
+            ['school_id' => '202202329', 'name' => 'Radoc, Cyrell Jane E.', 'date' => null],
+            ['school_id' => '202200385', 'name' => 'Rusiana, Shane Mark', 'date' => null],
+            ['school_id' => '202202332', 'name' => 'Sabejon, Matt Ian V.', 'date' => null],
+            ['school_id' => '202202334', 'name' => 'Santillan, Lorie Ann Mae T.', 'date' => null],
+            ['school_id' => '202120923', 'name' => 'Tubongbanua, Reden D.', 'date' => null], // absent
         ];
 
-        foreach ($names as $name) {
-            SectionA::create([
-                'school_id' => '000000000',
-                'name' => $name,
-                'date' => null,
-            ]);
+        foreach ($data as $item) {
+            SectionA::create($item);
         }
-
-        // SectionA::create([
-        //     [
-        //         'school_id' => '202101696',
-        //         'name' => 'Almirante, Jann Louie Q.',
-        //         'date' => null,
-        //     ],
-        //     [
-        //         'school_id' => '202203013',
-        //         'name' => 'Arceño, Jayson E.',
-        //         'date' => null,
-        //     ],
-        //     [
-        //         'school_id' => '202102022',
-        //         'name' => 'Austero, Crank Neil R.',
-        //         'date' => null,
-        //     ],
-        //     [
-        //         'school_id' => '202202286',
-        //         'name' => 'Bantillo, Phoebe Mariz Q.',
-        //         'date' => null,
-        //     ],
-        //     [
-        //         'school_id' => '202120854',
-        //         'name' => 'Bondad, Don Marc P.',
-        //         'date' => null,
-        //     ],
-        //     [
-        //         'school_id' => '201905138',
-        //         'name' => 'Cafino, Randy S.',
-        //         'date' => null,
-        //     ],
-        // ]);
     }
 }
--- database/seeders/AttendanceCSeeder.php
+++ database/seeders/AttendanceCSeeder.php
@@ -13,35 +13,32 @@ class AttendanceCSeeder extends Seeder
      */
     public function run(): void
     {
-        $names = [
-            'Mary Joy Abria',
-            'Gonzales, Christian P.',
-            'Gomez, Dino C.',
-            'isaac fabello',
-            'John Clyde Garcia',
-            'Judy Ann Gravoso',
-            'Abrenica Jay Jovi James',
-            'Ajoc, John Lloyd',
-            'Santonil Jun Cyril A.',
-            'Macasukit, Luiguie',
-            'Arjay Narbas',
-            'Aldenese Nicole',
-            'Carlton James Ojales',
-            'Alvarez Rachel',
-            'Veradio, Rex Mikheal',
-            'Tayros, Sean Ivan',
-            'Descatamiento, Shiela Mae A.',
-            'BERNAL, TAMARA FAYE',
-            'Joshua Villaflores',
-            'Kennith Khember “VILLEGAS, KENNITH A” Villegas',
+        $data = [
+            ['school_id' => '202202308', 'name' => 'Abrenica, Jay Jovi James B.', 'date' => null],
+            ['school_id' => '202202280', 'name' => 'Abria, Mary Joy A.', 'date' => null],
+            ['school_id' => '202202282', 'name' => 'Ajoc, John Lloyd P.', 'date' => null],
+            ['school_id' => '202202283', 'name' => 'Aldenese, Nicole A.', 'date' => null],
+            ['school_id' => '202121427', 'name' => 'Alvarez, Rachel A.', 'date' => null],
+            ['school_id' => '202200364', 'name' => 'Bernal, Tamara Faye C.', 'date' => null],
+            ['school_id' => '202303536', 'name' => 'Custodio, John Michael L.', 'date' => null], // absent
+            ['school_id' => '202200371', 'name' => 'Descatamiento, Shiela Mae A.', 'date' => null],
+            ['school_id' => '202303545', 'name' => 'Fabello, Isaac G.', 'date' => null],
+            ['school_id' => '202202306', 'name' => 'Garcia, John Clyde B.', 'date' => null],
+            ['school_id' => '202200376', 'name' => 'Gomez, Dino C.', 'date' => null],
+            ['school_id' => '202202307', 'name' => 'Gonzales, Christian P.', 'date' => null],
+            ['school_id' => '202120878', 'name' => 'Gravoso, Judy Ann O.', 'date' => null],
+            ['school_id' => '202200377', 'name' => 'Macasukit, Luiguie C.', 'date' => null],
+            ['school_id' => '202120890', 'name' => 'Mendez, Angel Mae', 'date' => null], // absent
+            ['school_id' => '202202320', 'name' => 'Narbas, Arjay Ejercito', 'date' => null],
+            ['school_id' => '201901647', 'name' => 'Santonil, Jun Cyril A.', 'date' => null], // absent
+            ['school_id' => '202202339', 'name' => 'Tayros, Sean Ivan', 'date' => null],
+            ['school_id' => '202002622', 'name' => 'Veradio, Rex Mikheal', 'date' => null],
+            ['school_id' => '202200390', 'name' => 'Villaflores, Joshua S.', 'date' => null],
+            ['school_id' => '202202344', 'name' => 'Villegas, Kennith Khember A.', 'date' => null],
         ];
 
-        foreach ($names as $name) {
-            SectionC::create([
-                'school_id' => '000000000', // Adjust the school_id as needed
-                'name' => $name,
-                'date' => null,
-            ]);
+        foreach ($data as $item) {
+            SectionC::create($item);
         }
     }
 }
